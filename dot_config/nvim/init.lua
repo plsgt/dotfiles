781b1adaafd7957b99227ad6455b66e9ec204f43
Saugata Paul <saugatapaul007@gmail.com>
2024-02-22 21:31:58 +0530

    Switched to lazy nvim and other minor changes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,23 +1,2 @@
-require('plugins')
-require('mason-config')
-require('clangd')
-require('code-completion')
-require('file-explorer')
---require('styling')
-require('statusbar')
-
-if (os.getenv("TERM_PROGRAM")) ~= 'Apple_Terminal' then
-	vim.cmd[[colorscheme dracula]]
-end
-
-vim.opt.compatible=false
-vim.opt.wrap=true
-vim.opt.showmatch=true
-vim.opt.ignorecase=true
-vim.opt.autoindent=true
-vim.opt.number=true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
-vim.opt.softtabstop=4
-vim.opt.guicursor='a:ver25-Cursor'
-vim.opt.swapfile=false
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ dot_config/nvim/lua/plugins/colorcheme.lua
@@ -0,0 +1,15 @@
+return {
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+
+	-- add base16 colorschemes
+	{ "RRethy/nvim-base16" };
+
+	-- Configure LazyVim to load gruvbox
+	--{
+	--	"LazyVim/LazyVim",
+	--	opts = {
+	--		colorscheme = "gruvbox",
+	--	},
+	--}
+}
